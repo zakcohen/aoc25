dd894390920192c32a60cd17d9971e1d59fd4060
Zak Cohen <zakcohen@gmail.com>
2025-12-06 22:41:17 -0800

    Day 4 part 2

--- day4.py
+++ day4.py
@@ -1,4 +1,5 @@
 from grid import Grid
+from point import Point
 
 def part1():
     grid = Grid()
@@ -19,9 +20,37 @@ def part1():
     print("part 1: ", accessibleRolls)
     print("================================================")
 
+def getAccessibleRolls(grid):
+    accessibleRolls = []
+    for x in range(grid.width()):
+        for y in range(grid.height()):
+            if grid.get(x, y) == '@':
+                neighborRollCount = 0
+                for neighbor in grid.get_neighbors_8(x,y):
+                    if grid.get(neighbor.x, neighbor.y) == '@':
+                        neighborRollCount += 1
+                if neighborRollCount < 4:
+                    accessibleRolls.append(Point(x, y))
+    return accessibleRolls
 
 def part2():
-    pass
+    grid = Grid()
+    with open('static/day4.txt', 'r') as file:
+        for line in file:
+            line = line.strip()
+            grid.data.append(list(line))
+
+    removedRolls = 0
+    while True:
+        rolls = getAccessibleRolls(grid)
+        if len(rolls) == 0:
+            break
+        for roll in rolls:
+            grid.set(roll.x, roll.y, '.')
+            removedRolls += 1
+        rolls.clear()
+    print("part 2: ", removedRolls)
+    print("================================================")
 
 def main():
     part1()
