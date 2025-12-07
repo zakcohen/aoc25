cc5f6d22952777b808558a076c4d63e4f9f001e5
Zak Cohen <zakcohen@gmail.com>
2025-12-06 21:30:14 -0800

    Day 2 part 2

--- day2.py
+++ day2.py
@@ -1,24 +1,41 @@
+import textwrap
 
 def part1():
+    invalidCount = 0
+    for numRange in ranges():
+        invalidCount += sum([num for num in numRange if invalid(num)])
+    print("part 1: ", invalidCount)
+    print("================================================")
+
+def ranges():
     with open('static/day2.txt', 'r') as file:
         for line in file:
             line = line.strip()
-
-            invalidCount = 0
             ranges = line.split(',')
             for numRange in ranges:
                 numRange = numRange.split('-')
-                invalidCount += sum([num for num in range(int(numRange[0]), int(numRange[1]) + 1) if invalid(num)])
-            print("part 1: ", invalidCount)
-            print("================================================")
+                yield range(int(numRange[0]), int(numRange[1]) + 1)
 
 def invalid(num):
     strNum = str(num)
     midPoint = len(strNum) // 2
     return strNum[:midPoint] == strNum[midPoint:]
 
+def invalidPart2(num):
+    strNum = str(num)
+    for pieceLength in range(1, len(strNum) // 2 + 1):
+        pieces = textwrap.wrap(strNum, pieceLength)
+        if len(pieces) >= 2 and all(piece == pieces[0] for piece in pieces):
+            return True
+    return False
+
+
 def part2():
-    pass
+    invalidCount = 0
+    for numRange in ranges():
+        invalidCount += sum([num for num in numRange if invalidPart2(num)])
+    print("part 2: ", invalidCount)
+    print("================================================")
 
 def main():
     part1()
