7206ed0deb50dc5fb3e69b7b4dabb6814b9f4735
Zak Cohen <zakcohen@gmail.com>
2025-12-06 22:19:13 -0800

    Day 3 part 2

--- day3.py
+++ day3.py
@@ -12,8 +12,21 @@ def part1():
         print("part 1: ", maxPower)
         print("================================================")
 
+def maxJoltage(remainingBank, remainingCells):
+    if remainingCells == 0:
+        return ""
+    nextDigit = max(remainingBank) if remainingCells == 1 else max(remainingBank[:-(remainingCells - 1)])
+    nextDigitIndex = remainingBank.index(nextDigit)
+    return nextDigit + maxJoltage(remainingBank[nextDigitIndex+1:], remainingCells - 1)
+
 def part2():
-    pass
+    with open('static/day3.txt', 'r') as file:
+        maxPower = 0
+        for line in file:
+            bank = line.strip()
+            maxPower += int(maxJoltage(bank, 12))
+        print("part 2: ", maxPower)
+        print("================================================")
 
 def main():
     part1()
